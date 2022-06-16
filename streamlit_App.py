0810225d96a41cb624340a2695ca40a3e1a02746
KavitKumar <92976767+KavitKumar@users.noreply.github.com>
2022-06-16 06:41:14 -0400

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -6,7 +6,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 selectfruit = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit),['Apple'])
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),[4,8])
 showfruit = my_fruit_list.loc(selectfruit)
-streamlit.dataframe(showfruit)
 streamlit.dataframe(my_fruit_list)
-
-
+streamlit.dataframe(showfruit)
