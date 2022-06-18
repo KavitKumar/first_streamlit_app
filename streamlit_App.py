c734848c8e9014f6456534c974238bd019431915
KavitKumar <92976767+KavitKumar@users.noreply.github.com>
2022-06-18 15:34:13 -0400

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -5,7 +5,8 @@ import requests
 
 responce = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(responce.json())
-
+normalize = pandas.json_normalize(responce.json())
+streamlit.dataframe(normalize)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list= my_fruit_list.set_index('Fruit')
