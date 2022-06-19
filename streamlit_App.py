de731d5ccb33e7396954f5621609de8ac5185d6d
KavitKumar <92976767+KavitKumar@users.noreply.github.com>
2022-06-19 15:47:37 -0400

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import snowflake.connector
+
 
 import requests
 user_input = streamlit.text_input("eneter your fruit","Kiwi")
@@ -18,10 +18,5 @@ showfruit = my_fruit_list.loc[selectfruit]
 streamlit.dataframe(showfruit)
 streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(showfruit)
+import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
