a63c9d31172b495fb761e592e0b3b40e26f1293b
KavitKumar <92976767+KavitKumar@users.noreply.github.com>
2022-06-19 16:23:41 -0400

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -22,7 +22,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("insert into fruit_load_list values ('from streamlit');")
 my_data_row = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
